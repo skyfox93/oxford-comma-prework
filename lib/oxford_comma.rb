a700169bd336db4068b433bf519abbdc517ba693
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 00:34:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-last=array.pop
-with_and="and "<< last
-array.push(with_and)
-array.join(", ")
+if(array.length>1)  
+  last=array.pop
+  with_and="and "<< last
+  array.push(with_and)
+  if array.length==2
+    array.join(" ")
+  else array.join(", ")
+end
 end
