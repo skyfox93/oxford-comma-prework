4d9c7abfa13adfd8f41d145e0b88d60d7c56dd96
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 00:38:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+if(array.length>1)  
+  last=array.pop
+  with_and="and "<< last
+  array.push(with_and)
+  if array.length==2
+    array.join(" ")
+  else array.join(", ")
+  end
+else array.join(" ")
+end
 end
